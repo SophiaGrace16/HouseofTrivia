c7768d37e3777e32dd3e05ff32d02a429658e709
SophiaGrace16 <sophia.g.white@outlook.com>
2020-09-10 13:42:26 -0400

    Added a bundler and gem file

---
+++ house_of_trivia/lib/house_of_trivia.rb
@@ -0,0 +1,6 @@
+require "house_of_trivia/version"
+
+module HouseOfTrivia
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ house_of_trivia/lib/house_of_trivia/version.rb
@@ -0,0 +1,3 @@
+module HouseOfTrivia
+  VERSION = "0.1.0"
+end
